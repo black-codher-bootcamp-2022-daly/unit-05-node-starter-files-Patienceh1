f5b368ce4b0d73793d8bc59f608a7a9b4759981c
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-23 21:08:11 +0000

    basic-server

--- simple-nodejs-server/index.js
+++ simple-nodejs-server/index.js
@@ -6,8 +6,11 @@ const port = 8080
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({
+    greeting: 'hey',
+    name: 'Patience'
+  }))
 })
 
 server.listen(port, hostname, () => {
