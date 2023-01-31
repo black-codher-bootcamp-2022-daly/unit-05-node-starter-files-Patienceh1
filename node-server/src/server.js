3e8b72651f7bcf1576b5f3937409434069862665
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-31 20:00:43 +0000

    working-profiles-json

--- node-server/src/server.js
+++ node-server/src/server.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
@@ -8,25 +11,50 @@ app.get("/", (req, res) => {
 /*
 Add a route to the server.js file with code that will return the contents 
 of the JSON file profiles.json when a user visits the following URL:
-
 GET http://localhost:8080/profile
-
 Replace ... with the correct path
 */
 app.get("/profiles", (req, res) => {
-  response.writeHeader(200, { "Content-Type": "application/json" })
+  fs.readFile("../src/models/profiles.json", function (err, json) {
+    if (err) {
+      throw err;
+    }
+
+    // The slide referenced res.writeHead(), but I'm replacing that problematic call with
+    // modern Express method chaining to make the logic more readable.
+    // We set the Content-Type header and the status code, send the body and finally end the response.
+    res.set("Content-Type", "application/json").status(200).send(json).end();
+  });
 });
 
-  /*
+/*
   Add a route that returns a list of the profile names and bio 
   from the server as HTML elements on the endpoint below:
-
   GET http://localhost:8080/profiles/view 
-
   Replace ... with the correct path
   */
-app.get("...", (req, res) => {
+app.get("/profiles/view", (req, res) => {
+  fs.readFile("./models/profiles.json", function (err, json) {
+    if (err) {
+      throw err;
+    }
 
+    res
+      .set("Content-Type", "text/html")
+      .status(200)
+      .send(
+        // When we call fs.readFile(), we get a string that we convert into a JSON object
+        // (an array, in this case) using JSON.parse(). We then call the reduce() array method
+        // to convert the array into a single string which we send back to the client and use string
+        // interpolation to append the markup generated for each element.
+        JSON.parse(json).reduce(
+          (previous, current) =>
+            `${previous}<h2>${current.firstname} ${current.lastname}</h2>\n<p>${current.bio}</p>\n`,
+          "<h1>Profiles</h1>\n"
+        )
+      )
+      .end();
+  });
 });
 
 app.listen(port, () => {
--- simple-nodejs-server/index.js
+++ simple-nodejs-server/index.js
@@ -1,6 +1,7 @@
 //An Example Node.js Application can be found here: https://nodejs.dev/en/learn/
 const http = require('http')
 
+// 127.0.0.1 always refers to your own machine
 const hostname = '127.0.0.1'
 const port = 8080
 
