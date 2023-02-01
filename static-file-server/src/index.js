0f3ff86938e0356f5504709ff0b79f92087c4d3e
Patience Hodzi <patiencehodzi@gmail.com>
2023-02-01 20:01:47 +0000

    express-path

--- static-file-server/src/index.js
+++ static-file-server/src/index.js
@@ -1,1 +1,13 @@
-/* Add your server code here*/
+var express = require("express");
+var app = express();
+var path = require('path');
+
+app.use("/content", express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html", { root: __dirname });
+});
+
+app.listen(8081);
+
